568aff63e9747805352fdd1902abb440eff25b1d
JungminKim1101 <54600334+JungminKim1@users.noreply.github.com>
2021-10-03 18:23:35 -0400

    Small Addition on Dashboard
    Just some small addition of layout and comments

--- modus-app/src/journalDashboard.js
+++ modus-app/src/journalDashboard.js
@@ -18,8 +18,15 @@ function App() {
           </div>
         </a>
       </header>
-      <section className="topDashboard" id="topDashboard"></section>
-      <section className="mainDashboard" id="mainDashboard"></section>
+      <section className="topDashboard" id="topDashboard">
+        <h2>Hi User. How are you today?</h2>
+        /* Used to write a new entry */ /* Add an icon also with the button */
+        <button className="writeButton">Write New Entry</button>
+      </section>
+      <section className="mainDashboard" id="mainDashboard">
+        <h3>Recent Entries</h3>
+        /* list of the recent entries */
+      </section>
     </div>
   );
 }
