b2d8cd14e44996b026721e336dfa47f7d8f329dc
JungminKim1101 <54600334+JungminKim1@users.noreply.github.com>
2021-10-10 18:01:14 -0400

    FAQ css
    just a small update on CSS for faq page

--- modus-app/src/faq.js
+++ modus-app/src/faq.js
@@ -5,23 +5,9 @@ import "./faq.css";
 function App() {
   return (
     <div className="dashboard">
-      <header>
-        <a href="#" className="sidebar">
-          <div className="navigation">
-            /* change the naviagtion to icons */ /*navigation is on the sidebar
-            and logout will be on the way bottom */
-            <a href="#Profile">Profile</a>
-            <a href="#dashboard">Dashboard</a>
-            <a href="#library">Library</a>
-            <a href="#analysis">Analysis</a>
-            <a href="#question">FAQ</a>
-            <a href="#logout">Logout</a>
-          </div>
-        </a>
-      </header>
       <section className="faq" id="faq">
         <h2>FAQ</h2>
-        /* line break */ /* tabs that open up with answers inside there */
+        <Divider classname="divider" />
         <div className="question1">
           <h3>Question</h3>
           <text>
