bf5610e1f669db88404f32c10e6141e353486a5c
JungminKim1101 <54600334+JungminKim1@users.noreply.github.com>
2021-10-14 21:51:41 -0400

    Change in files
    Moving Files around

--- modus-app/src/pages/Faq.js
+++ modus-app/src/pages/Faq.js
@@ -1,10 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./Faq.css";
 
-function Faq() {
+function App() {
   return (
-    <h1>FAQ</h1>
+    <div className="dashboard">
+      <section className="faq" id="faq">
+        <h2>FAQ</h2>
+        <Divider classname="divider" />
+        <div className="question1">
+          <h3>Question</h3>
+          <text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </text>
+        </div>
+        <div className="question2">
+          <h3>Question</h3>
+          <text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </text>
+        </div>
+        <div className="question3">
+          <h3>Question</h3>
+          <text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </text>
+        </div>
+        <div className="question4">
+          <h3>Question</h3>
+          <text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </text>
+        </div>
+      </section>
+    </div>
   );
 }
 
-export default Faq;
+export default App;
