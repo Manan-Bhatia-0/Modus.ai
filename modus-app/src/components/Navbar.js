266d1c397b21e720265e89f51354e122332f1bbd
Jisoo Kim <jisookim@Jisoos-MacBook-Air.local>
2021-10-15 07:58:16 -0400

    Debugged nav bar spacing

--- modus-app/src/components/Navbar.js
+++ modus-app/src/components/Navbar.js
@@ -1,22 +1,23 @@
 
 import React from "react";
+import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import './navbar.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 function NavBar() {
     return (
-        <Nav bsPrefix="navbar" defaultActiveKey="/" className="flex-column">
+        <Navbar defaultActiveKey="/" className="flex-column">
             <div>
-            <Nav.Link bsPrefix="text" href="/">Dashboard</Nav.Link>
-            <Nav.Link bsPrefix="text" href="/profile">Profile</Nav.Link>
-            <Nav.Link bsPrefix="text" href="/write">New Page</Nav.Link>
-            <Nav.Link bsPrefix="text" href="/library">Library</Nav.Link>
-            <Nav.Link bsPrefix="text" href="/analysis">Analysis</Nav.Link>
-            <Nav.Link bsPrefix="text" href="/faq">FAQ</Nav.Link>
-            <Nav.Link bsPrefix="text" href="/logout">Logout</Nav.Link>
+                <Nav.Link href="/">Dashboard</Nav.Link>
+                <Nav.Link href="/profile">Profile</Nav.Link>
+                <Nav.Link href="/write">New Page</Nav.Link>
+                <Nav.Link href="/library">Library</Nav.Link>
+                <Nav.Link href="/analysis">Analysis</Nav.Link>
+                <Nav.Link href="/faq">FAQ</Nav.Link>
+                <Nav.Link href="/logout">Logout</Nav.Link>
             </div>
-        </Nav>
+        </Navbar>
     );
 
 }
