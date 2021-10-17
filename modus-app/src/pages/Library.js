8fd201f8b3d84aef26cfc3fcf71710480dd588f4
Jisoo Kim <jisookim@Jisoos-MacBook-Air.local>
2021-10-17 19:12:32 -0400

    updated Library page

--- modus-app/src/pages/Library.js
+++ modus-app/src/pages/Library.js
@@ -1,9 +1,58 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import LibraryCard from "../components/LibraryCard";
+import {Grid, Divider, Pagination, Select, MenuItem, InputLabel} from '@mui/material'
+import "../components/LibraryCard.css"
 
 function Library() {
+  const [sort, setSort] = React.useState('');
+
+  const handleChange = (event) => {
+    setSort(event.target.value);
+  }
+
   return (
-    <h1>Library</h1>
+    <div
+      style={{margin: "5rem"}}
+    >
+      <Grid container>
+        <Grid item xs={10}>
+          <h1 
+            style={{
+              marginTop: "10rem"
+            }}>
+              User's Library
+          </h1>
+        </Grid>
+        <Grid item xs style={{
+              marginTop: "9rem"
+            }}
+            >
+          <InputLabel id="sort-by-label">Sort by</InputLabel>
+          <Select
+            labelId="sort-by-label"
+            id="sort-by"
+            value={sort}
+            label="Sort by"
+            onChange={handleChange}
+            style={{minwidth: 100}}
+          >
+            <MenuItem value={"dateSaved"}>Date Saved</MenuItem>
+            <MenuItem value={"alpha"}>Alphabetical</MenuItem>
+            <MenuItem value={"dateCreated"}>Date Created</MenuItem>
+          </Select>
+        </Grid>
+      </Grid>
+      
+      <Divider style={{width: "65rem"}}/>
+      <Grid container direction="column" style={{marginTop: "2rem"}}>
+        <Grid item>
+          <LibraryCard />
+        </Grid>
+        <Grid container item justifyContent="center">
+          <Pagination count={2} />
+        </Grid>
+      </Grid>
+    </div>
   );
 }
 
