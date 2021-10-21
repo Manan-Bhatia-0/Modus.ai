bcf24dcb430daccca76eaca71f6c55dc9e49ba12
JungminKim1101 <54600334+JungminKim1@users.noreply.github.com>
2021-10-21 09:58:21 -0400

    Basic HTML of create and login
    Basic Html of Create page and login page

--- modus-app/src/pages/Login.js
+++ modus-app/src/pages/Login.js
@@ -3,18 +3,26 @@ import { GoogleLogin} from react-google-Login;
 import "./Login.css";
 
 export function Login() {
-  <div>
-    <GoogleLogin
+  return (
+    <div>
+      <GoogleLogin
         clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
         buttonText="Log in with Google"
         cookiePolicy={'single_host_origin'}
-    />
-    <h2>Log in </h2>
-    <Input type="email" placeholder="Email Address " />
-    <Input type="password" placeholder="Password " />
-    <a href="#">Log in</a>
-    <a href="#">Forget your password?</a>
-    <a href="#">Create account</a>
-    <a href="#">Keep me signed in</a>
-  </div>;
+      />
+      <h2>Log in </h2>
+      <Input type="email" placeholder="Email Address " />
+      <Input type="password" placeholder="Password " />
+      <button className="login">Log in</button>
+      <a href="#">Forget your password?</a>
+      <a href="#">Create account</a>
+      <Input
+        type="checkbox"
+        name="save_login_state"
+        label="Keep me signed in"
+        checked={this.state.saveLoginState}
+        onChange={this.toggleLoginState.bind(this)}
+      />
+    </div>
+  );
 }
