f2a5c5f82e206c4d5d61e41c544a00d188493d47
JungminKim1101 <54600334+JungminKim1@users.noreply.github.com>
2021-10-23 18:17:16 -0400

    Setup register page
    Finished the html part of register page

--- modus-app/src/pages/CreateAccount.js
+++ modus-app/src/pages/CreateAccount.js
@@ -1,16 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import {
+  auth,
+  registerWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithGoogle,
+} from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 import "./CreateAccount.css";
 
-export function Login() {
+export function CreateAccount() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [user, loading, error] = useAuthState(auth);
+  const history = useHistory();
+  const register = () => {
+    if (!name) alert("Please enter name");
+    registerWithEmailAndPassword(name, email, password);
+  };
+  useEffect(() => {
+    if (loading) return;
+    if (user) history.replace("/dashboard");
+  }, [user, loading]);
   return (
     <div>
       <h2>Create Account</h2>
-      <Input type="firstname" placeholder="First Name " />
-      <Input type="lastname" placeholder="Last Name " />
-      <Input type="email" placeholder="Email Address " />
-      <Input type="password" placeholder="Password " />
-      <Input type="confirm" placeholder="Confirm Password " />
-      <button className="login">Sign Up</button>
+      <input
+        type="text"
+        placeholder="Full Name "
+        className="registerName"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Email Address "
+        className="registerEmail"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password "
+        className="registerPassword"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <input type="confirm" placeholder="Confirm Password " />
+      <button className="registerButton" onClick={register}>
+        Sign Up
+      </button>
     </div>
   );
 }
