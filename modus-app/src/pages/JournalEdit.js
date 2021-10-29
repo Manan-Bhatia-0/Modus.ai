cd70ae6eaf9623d2eba9cbc205e65f8488854893
bhatia47 <bhatia47@purdue.edu>
2021-10-29 12:08:54 -0400

    fix bug that crashed app

--- modus-app/src/pages/JournalEdit.js
+++ modus-app/src/pages/JournalEdit.js
@@ -7,33 +7,49 @@ import TextField from "@mui/material/TextField";
 import "./journalEdit.css";
 import {useRef } from 'react'
 import {useState} from 'react' 
-import {getJournalEntries, saveJournalEntry, submitJournalEntry} from "../firebase";
+import {deleteCurrentUser, getJournalEntries, saveJournalEntry, submitJournalEntry} from "../firebase";
 
 function JournalEdit() {
-  const [title, setTitle] = useState('') 
-  const [textfield, setTextField] = useState('')
+  const [myValue, setValue] = useState('') 
+  const [textfield, setValue2] = useState('')
+  const [value, setValue3] = useState('')
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const entry = { myValue, textfield };
+    console.log(entry);
+    console.log(document.activeElement.getAttribute('value'));
+    submitfn(myValue, textfield) 
+  }
+  // const onSave = handleSubmit(e => {
+  //   e.submitAction = 'decline'
+  //   console.log("save")
+  // })
+
+ // <form onSubmit={handleSubmit}> 
 
   return (
     <div className="dashboard">
       <section className="journalEdit" id="journalEdit">
         <h2>Journal Entry Name #342</h2>
         <h4>Date</h4>
-        <form >
+        <form onSubmit={handleSubmit}>
         <div className="title">
           <h3>Title</h3>
-          <TextField value={title} 
+          <TextField value={myValue} 
 			onChange={(e) => setValue(e.target.value)} />
         </div>
         <div className="entryContent">
           <h3>Entry Content</h3>
           <SunEditor value={textfield} 
-      onChange={setTextField}
+      onChange={setValue2}
+      // onChange={(e) => setValue2(e.target.value)} 
       setOptions={{ height: "500px", width: "1000px" }} />
         </div>
         <div className="buttons">
           <button className="save"
-          onClick={() => saveJournalEntry(title, textfield)}>Save</button>
-          <button className="submit" onClick={() => submitJournalEntry(title, textfield)}>Submit</button>
+          onClick={() => saveJournalEntry(myValue, textfield)}>Save</button>  
+          <button className="submit" onClick={() => submitJournalEntry(myValue, textfield)}>Submit</button>
         </div>
         </form>
       </section>
@@ -41,10 +57,22 @@ function JournalEdit() {
   );
 }
 
-/* function submit() // no ';' here
+function submit() // no ';' here
 {
     var elem = document.getElementsByClassName("submit2");
     console.log(elem.value);
-} */
+}
+
+
+function submitfn(myValue, textfield) {
+
+  console.log(myValue)
+  console.log(textfield)
+  // var title = document.getElementsByClassName('title').value;
+  // var entry_content = document.getElementsByClassName('entryContent').useRef;
+  console.log('Client-side code running');
+  // console.log(title)
+  // console.log(entry_content)
+}
 
 export default JournalEdit;
