b50d1ea1219765f8597a80780068b82e25b56184
rashmi-ananth <66443393+rashmi-ananth@users.noreply.github.com>
2021-11-04 19:47:29 -0400

    added popup message on delete button

--- modus-app/src/pages/Profile.js
+++ modus-app/src/pages/Profile.js
@@ -1,12 +1,45 @@
-/* eslint-disable */ 
+/* eslint-disable */
 import React from "react";
 import ReactDOM from "react-dom";
+import { deleteCurrentUser } from "../firebase";
+import { Link, useHistory } from "react-router-dom";
+import { confirm } from "react-confirm-box";
+
+const optionsWithLabelChange = {
+  closeOnOverlayClick: false,
+  labels: {
+    confirmable: "Confirm",
+    cancellable: "Cancel",
+  },
+};
+
+const onClick = async (options) => {
+  const result = await confirm(
+    "Are you sure you want to delete your account?",
+    options
+  );
+  if (result) {
+    deleteCurrentUser();
+    history.push("/");
+    console.log("Deleted User!");
+    return;
+  }
+  console.log("Deleting Cancelled");
+};
 
 function Profile() {
+  const history = useHistory();
   return (
     <div>
       <h1>Profile</h1>
-      <button>Delete Profile</button>
+      <button
+        onClick={() => {
+          onClick(optionsWithLabelChange);
+        }}
+      >
+        {" "}
+        Delete Profile{" "}
+      </button>
     </div>
   );
 }
