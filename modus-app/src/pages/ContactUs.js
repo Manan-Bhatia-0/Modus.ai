152b61a4a08cbf998a1c7e305584255544ec1142
mihirakk <krish134@purdue.edu>
2021-12-01 18:56:46 -0500

    contact us merge

---
+++ modus-app/src/pages/ContactUs.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import emailjs from "emailjs-com";
+
+const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [emailSent, setEmailSent] = useState(false);
+
+  const submit = () => {
+    if (name && email && message) {
+      const serviceId = "service_id";
+      const templateId = "template_id";
+      const userId = "user_id";
+      const templateParams = {
+        name,
+        email,
+        message,
+      };
+
+      emailjs
+        .send(serviceId, templateId, templateParams, userId)
+        .then((response) => console.log(response))
+        .then((error) => console.log(error));
+      setName("");
+      setEmail("");
+      setMessage("");
+      setEmailSent(true);
+    } else {
+      alert("Some fields are missing!");
+    }
+  };
+
+  return (
+    <div id="contact-form">
+      <input
+        type="text"
+        placeholder="Name "
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <textarea
+        placeholder="Message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      ></textarea>
+      <button onClick={submit}>Send</button>
+      <span className={emailSent ? "visible" : null}>
+        Thank you so much for the feedback!
+      </span>
+    </div>
+  );
+};
+
+export default Contact;
