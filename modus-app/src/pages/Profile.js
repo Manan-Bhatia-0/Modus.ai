2fa05bcaab7910f043e4a594ce365a6e90533305
JungminKim1101 <54600334+JungminKim1@users.noreply.github.com>
2021-12-02 18:51:25 -0500

    navbar + profile update
    updated profile to test gender and birthday

--- modus-app/src/pages/Profile.js
+++ modus-app/src/pages/Profile.js
@@ -1,10 +1,10 @@
 /* eslint-disable */
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import { deleteCurrentUser} from "../firebase";
+import { deleteCurrentUser } from "../firebase";
 import { Link, useHistory } from "react-router-dom";
 import { confirm } from "react-confirm-box";
-import { getUserName, editProfile} from "../firebase";
+import { getUserName, editProfile } from "../firebase";
 
 const optionsWithLabelChange = {
   closeOnOverlayClick: false,
@@ -32,27 +32,54 @@ function Profile() {
   const [name, setName] = useState("");
   const [age, setAge] = useState("");
   const [gender, setGender] = useState("");
+  const [birthday, setBirthday] = useState("");
   // var select = document.getElementById('gender');
   // var gender = select.options[select.selectedIndex].value;
   //const history = useHistory();
   return (
     <div class="profile container">
       <h1 class="text-primary">Edit Profile</h1>
-        <label class="name-field">Name:</label>
-        <input class="form-control" type="text" value={name} onChange = {(e) => setName(e.target.value)}/>
-        <label class="name-field">Age:</label>
-        <input class="form-control" type="text" value={age} onChange = {(e) => setAge(e.target.value)}/>
-        <label class="name-field">Gender:</label>
-        <input class="form-control" type="text" value={gender} onChange = {(e) => setGender(e.target.value)}/>
-        {/* <select id="gender" class="form-control" >
-                  <option value="Female">Female</option>
-                  <option value="Male">Male</option>
-                  <option value="Other">Other</option>
-        </select> */}
-        <button className = "saveProfile" onClick = {() => editProfile(name, gender, age)}>
+      <label class="name-field">Name:</label>
+      <input
+        class="form-control"
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <label class="age-field">Age:</label>
+      <input
+        class="form-control"
+        type="text"
+        value={age}
+        onChange={(e) => setAge(e.target.value)}
+      />
+      <label class="gender-field">Gender:</label>
+      {/* <input class="form-control" type="text" value={gender} onChange = {(e) => setGender(e.target.value)}/> */}
+      <select
+        id="genderForm"
+        class="form-control"
+        value={gender}
+        onChange={(e) => setGender(e.target.value)}
+      >
+        <option value="Female">Female</option>
+        <option value="Male">Male</option>
+        <option value="Other">Other</option>
+      </select>
+      <label class="birthday-field">Birthday:</label>
+      /*{" "}
+      <input
+        class="form-control"
+        type="date"
+        value={birthday}
+        onChange={(e) => setBirthday(e.target.value)}
+      />
+      <button
+        className="saveProfile"
+        onClick={() => editProfile(name, gender, age, birthday)}
+      >
         Save Profile
-       </button>
-       <button
+      </button>
+      <button
         right="100px"
         justifyContent="center"
         alignItems="center"
