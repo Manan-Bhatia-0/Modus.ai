c54764320cdf7676670bcc2f435ff2e1a39fc8ef
Jisoo Kim <jisookim@pal-nat186-159-116.itap.purdue.edu>
2021-12-03 15:51:19 -0500

    Fixed spacing and formatting for text fields

--- modus-app/src/pages/ContactUs.js
+++ modus-app/src/pages/ContactUs.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Grid } from '@mui/material';
 import emailjs from "emailjs-com";
 
 const Contact = () => {
@@ -33,34 +34,49 @@ const Contact = () => {
 
   return (
     <div id="contact-form">
-      <h2>
-      <br></br>
-        Contact Us
-        </h2>
-      <br></br>
-      <textarea
-        type="text"
-        placeholder="Name "
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <br></br>
-      <textarea
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <br></br>
-      <textarea
-        placeholder="Message"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      ></textarea>
-      <button className="contactUsButton" onClick={submit}>Send</button>
-      <span className={emailSent ? "visible" : null}>
-       <br></br> <br></br> <br></br> <br></br>Thank you so much for the feedback!
-      </span>
+      <Grid container direction="column">
+        <Grid item>
+          <h2 style={{marginTop: "3rem", marginBottom: "3rem"}}>
+            Contact Us
+          </h2>
+        </Grid>
+        <Grid item container>
+          <Grid item>
+            <textarea
+              type="text"
+              style={{width: 250}}
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </Grid>
+          <Grid item style={{marginLeft: "2rem"}}>
+            <textarea
+              type="email"
+              style={{width: 400}}
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Grid>
+        </Grid>
+        <Grid item>
+          <textarea
+            placeholder="Message"
+            value={message}
+            style={{width: 683, height: 300}}
+            onChange={(e) => setMessage(e.target.value)}
+          ></textarea>
+        </Grid>
+        <Grid item>
+          <button className="contactUsButton" onClick={submit} style={{marginTop: 300, marginLeft: 500}}>Send</button>
+        </Grid>
+        {/* <Grid item>
+          <span className={emailSent ? "visible" : null}>
+            Thank you so much for the feedback!
+          </span>
+        </Grid> */}
+      </Grid>
     </div>
   );
 };
